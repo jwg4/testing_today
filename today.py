a1db2354ad2c884dffadf9f8cea74fddb13ce889
jwg4 <jwg4@users.noreply.github.com>
2018-02-23 16:10:27 +0000

    Missing import, arg order.

--- today.py
+++ today.py
@@ -1,5 +1,8 @@
+from datetime import date
+
+
 def use_today(f):
-    def f_using_today(*args, day=None, **kwargs):
+    def f_using_today(day=None, *args, **kwargs):
         if day is None:
             day = date.today()
         return f(*args, day, **kwargs)
