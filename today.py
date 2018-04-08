d1de78e40ca583cf5dec6dd04c0b1f6c6c491538
jwg4 <jwg4@users.noreply.github.com>
2018-04-08 21:17:36 +0000

    Use a keyword argument here.

--- today.py
+++ today.py
@@ -5,5 +5,5 @@ def use_today(f):
     def f_using_today(day=None, *args, **kwargs):
         if day is None:
             day = date.today()
-        return f(*args, day, **kwargs)
+        return f(*args, day=day, **kwargs)
     return f_using_today
